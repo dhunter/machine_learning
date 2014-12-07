5bc24fde9354a08d5e4e682847b0cf9e726ea0f8
Dylan Hunter <dylan@dylanhunter.com>
2014-12-07 16:26:06 -0500

    Added Euclidean distance-based similarity calculation.

--- recommendations.py
+++ recommendations.py
@@ -20,3 +20,24 @@ critics={'Lisa Rose':{'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
 		'The Night Listener': 3.0},
 	'toby':{'Snakes on a Plane': 4.5, 'Superman Returns': 4.0,
 		'You, Me and Dupree': 1.0}}
+
+
+# 	Calculate distance-based similarity score between two people.
+from math import sqrt
+def sim_distance(preferences,person_1,person_2):
+	#	Get the list of shared items
+	shared_items={}
+	for item in preferences[person_1]:
+		if item in preferences[person_2]:
+			shared_items[item]=1
+
+	#	If there are no common items, return 0.
+	if len(shared_items)==0: return 0
+
+	#	Add the squares of all the differences.
+	sum_of_squares=sum([pow(preferences[person_1][item]-
+		preferences[person_2][item])],2)
+		for item in preferences[person_1]
+			if item in preferences[person_2]
+
+	return 1/(1+sum_of_squares)
